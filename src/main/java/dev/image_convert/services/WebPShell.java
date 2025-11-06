24085de46ad7bd0e2432569953063e875d8171ac
Eric Macedo <ericthr42@gmail.com>
2025-11-06 09:51:37 -0300

    fix: WebPShell.java

--- src/main/java/dev/image_convert/services/WebPShell.java
+++ src/main/java/dev/image_convert/services/WebPShell.java
@@ -18,45 +18,37 @@ import org.springframework.shell.standard.ShellOption;
 @ShellComponent
 public class WebPShell {
 
-  @Autowired
-  ValidateQuality validateQuality;
+  @Autowired ValidateQuality validateQuality;
 
-  @Autowired
-  ValidateImage validateImage;
+  @Autowired ValidateImage validateImage;
 
-  @Autowired
-  ValidateParams validateParams;
+  @Autowired ValidateParams validateParams;
 
-  @Autowired
-  ConvertImage convertImage;
+  @Autowired ConvertImage convertImage;
 
   @ShellMethod(key = "webp", value = "Convert to WebP")
   public List<String> convertToWebP(
       @ShellOption(help = "Directory path", defaultValue = "") String d,
       @ShellOption(help = "Single file path", defaultValue = "") String s,
-      @ShellOption(help = "Quality", defaultValue = "0.75") String q
-  )
+      @ShellOption(help = "Quality", defaultValue = "0.75") String q)
       throws IOException, InvalidShellParamsException, MissingImageFileExcpetion {
-    var testPath = "/home/eric/Downloads/p.png";
-    var testPath2 = "/home/eric/Downloads/";
 
     validateParams.execute(s, d);
 
     float quality = validateQuality.execute(q);
 
     if (!s.isEmpty()) {
-      File imgFile = new File(testPath);
-      validateImage.singleFile(imgFile, testPath);
+      File imgFile = new File(s);
+      validateImage.singleFile(imgFile, s);
       return List.of(convertImage.convertSingleFile(imgFile, quality));
     }
 
     if (!d.isEmpty()) {
-      Path inputDir = Paths.get(testPath2);
+      Path inputDir = Paths.get(d);
       validateImage.directory(inputDir);
       return convertImage.convertDirectory(inputDir, quality);
     }
 
     return null;
   }
-
 }
